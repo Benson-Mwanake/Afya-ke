716be5e28046f3bbb04a439ebe76956e03e6685e
Benson <152067001+Benson-Mwanake@users.noreply.github.com>
2025-12-29 20:49:38 +0300

    Refactor Navbar component structure

--- client/src/hooks/layouts/Navbar.jsx
+++ client/src/hooks/layouts/Navbar.jsx
@@ -10,9 +10,8 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <div className="flex-shrink-0 flex items-center">
+          <div className="flex-shrink-0">
             <Link to="/" className="text-xl font-bold text-green-600">
-              <Heart className="w-6 h-6 mr-2 text-green-600 fill-green-600" />
               <span className="text-gray-900">Afya</span>Link
             </Link>
           </div>
